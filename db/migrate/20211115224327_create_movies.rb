7fe8e007b2d9897c54316c5514f8752ffac628b4
anonymous anonymous <asawari.agarwal@gmail.com>
2021-11-15 22:43:27 +0000

    Generate movies resource with ideas:scaffold

---
+++ db/migrate/20211115224327_create_movies.rb
@@ -0,0 +1,14 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :duration
+      t.text :description
+      t.string :poster
+      t.integer :director_id
+
+      t.timestamps
+    end
+  end
+end
